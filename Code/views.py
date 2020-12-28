aa80a842c431fe515a7102f35672b5e67913838f
Abdo Mosa <abdomosa598@gmail.com>
2020-12-28 14:11:05 +0200

    backend basic structure

--- Code/views.py
+++ Code/views.py
@@ -1,6 +1,14 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    return render_template("admin.html")
