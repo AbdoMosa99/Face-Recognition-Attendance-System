fc66d2797ff0c2800a877d799044d2ab40572975
Abdo Mosa <abdomosa598@gmail.com>
2021-03-20 20:00:07 +0200

    Back to version 2.0

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_bcrypt import Bcrypt
+
+
+# create and configure the application
+app = Flask(__name__)
+app.secret_key = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# link database and admin
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+# set up the api 
+from app.api import Registration, Attendance
+api = Api(app)
+api.add_resource(Attendance, '/api/attendance')
+api.add_resource(Registration, '/api/registration')
+
+# collect all code
+from app import models
+from app import views
+from app import api
+
+# create the admin views
+admin = Admin(app, index_view = views.MyAdminIndexView())
+admin.add_view(ModelView(models.Student, db.session))
+admin.add_view(ModelView(models.Doctor, db.session))
+admin.add_view(ModelView(models.Course, db.session))
+admin.add_view(ModelView(models.UniversityFaculty, db.session))
+admin.add_view(ModelView(models.Attendance, db.session))
+admin.add_view(ModelView(models.Log, db.session))
